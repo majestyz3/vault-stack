981e232295f7aadca57016c066d994194ac18daa
Yash Khemani <yash@khemani.com>
2025-06-13 21:29:44 -0400

    keep certs once they are generated

--- unstack.sh
+++ unstack.sh
@@ -2,14 +2,14 @@
 
 docker-compose down
 
-for i in 1 2; do
+for i in 1 2 3 4; do
   for j in 1 2 3; do
     rm -rfv vault${i}${j}/data/* vault${i}${j}/audit/* vault${i}${j}/snapshots/*
     # ls -lR vault${i}${j}
   done
 done
 
-rm -rfv certs/terraform.tfstate* certs/wildcard
+# rm -rfv certs/terraform.tfstate* certs/wildcard
 
 rm -rfv openldap/*
 
